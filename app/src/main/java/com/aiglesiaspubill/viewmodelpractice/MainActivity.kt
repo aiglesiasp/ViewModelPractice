42095e65554caab61e65d0fb710120cc0080dc3c
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-20 19:48:37 +0100

    ViewModel y LiveData arreglado en Observadores y Listeners

--- app/src/main/java/com/aiglesiaspubill/viewmodelpractice/MainActivity.kt
+++ app/src/main/java/com/aiglesiaspubill/viewmodelpractice/MainActivity.kt
@@ -8,18 +8,31 @@ import com.aiglesiaspubill.viewmodelpractice.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    val viewModel : MainActivityViewModel by viewModels()
+    private val viewModel : MainActivityViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        viewModel.numLiveData.observe(this) {
-            binding.tvTitle.text = it.toString()
+        setObservers()
+        setListeners()
+
+    }
+
+    private fun setObservers() {
+        with(viewModel) {
+            numLiveData.observe(this@MainActivity) {
+                binding.tvTitle.text = it.toString()
+            }
         }
-        binding.tvTitle.setOnClickListener {
-            viewModel.cambiarNumeroLiveData()
+    }
+
+    private fun setListeners() {
+        with(binding) {
+            tvTitle.setOnClickListener {
+                viewModel.cambiarNumeroLiveData()
+            }
         }
     }
 }

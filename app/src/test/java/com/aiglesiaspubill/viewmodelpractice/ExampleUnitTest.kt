7a2ffb48d5a1c2453109a09f496deebddc327e7b
aiglesiasp <iglesiaspubillaitor@gmail.com>
2022-12-21 12:52:03 +0100

    Example TEST

--- app/src/test/java/com/aiglesiaspubill/viewmodelpractice/ExampleUnitTest.kt
+++ app/src/test/java/com/aiglesiaspubill/viewmodelpractice/ExampleUnitTest.kt
@@ -10,8 +10,18 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    var viewModel = MainActivityViewModel()
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun testGetNum() {
+        var numInicial = viewModel.num
+        viewModel.cambiarNumero()
+        var numFin = viewModel.num
+        assertNotEquals(numInicial, numFin)
     }
 }
